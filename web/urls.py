286ba076969d09657e80f8f0eaf89d25d279e7eb
duero125 <duero@wp.pl>
2020-04-17 23:42:36 +0200

    przygotowanie do wgrywania modeli

--- web/urls.py
+++ web/urls.py
@@ -19,12 +19,19 @@ from django.contrib import admin
 from django.urls import path, include
 
 from .views import (
-    index, blog)
+    index, blog, blog2, post, projects, project, galery, contact)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('blog/', blog)
+    path('blog/', blog),
+    path('blog2/', blog2),
+    path('post/', post),
+    path('projects/', projects),
+    path('project/', project),
+    path('galery/', galery),
+    path('contact/', contact)
+
 
 ]
 
--- web/views.py
+++ web/views.py
@@ -5,3 +5,21 @@ def index(request):
 
 def blog(request):
     return render(request, 'Blog-posts.html', {})
+
+def blog2(request):
+    return render(request, 'Blog-posts-2.html', {})
+
+def post(request):
+    return render(request, 'Single-blog.html', {})
+
+def projects(request):
+    return render(request, 'Portfolio.html', {})
+
+def project(request):
+    return render(request, 'Portfolio-detail.html', {})
+
+def galery(request):
+    return render(request, 'Galery.html', {})
+
+def contact(request):
+    return render(request, 'Contact.html', {})
