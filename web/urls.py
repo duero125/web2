dad9b9dc548566b567d35caf8dfe02a396b3740c
duero125 <duero@wp.pl>
2020-04-17 19:08:32 +0200

    next commit

--- web/urls.py
+++ web/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from .views import (
+    index, blog)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog)
+
 ]
 
 
